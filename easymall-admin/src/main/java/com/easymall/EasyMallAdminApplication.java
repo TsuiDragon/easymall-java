9f4af4a30ccdec3edaf3e9f08423207d74edf13a
cuixiaolong <779997748@qq.com>
2026-03-09 18:50:30 +0800

    init: 项目初始化

---
+++ easymall-admin/src/main/java/com/easymall/EasyMallAdminApplication.java
@@ -0,0 +1,11 @@
+package com.easymall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyMallAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyMallAdminApplication.class, args);
+    }
+}
